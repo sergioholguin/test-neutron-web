385a96665bc50c4746f97e4c806edaad6a673911
Sergio Holguin <sholguinc@uni.pe>
2022-11-10 15:42:30 -0500

    Testing changes again

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ const Home = () => {
             <div className="container mx-auto w-4/5	md:max-w-[1100px] my-10 md:my-12">
                 <div className="flex md:flex-row items-center justify-center flex-col ">
                     <div className="md:w-3/5 flex flex-col items-center md:items-start ">
-                        <p className="text-[14px] md:text-[16px] text-justify text-[#7a7a7a]">PROMITIA WAS HERE</p>
+                        <p className="text-[14px] md:text-[16px] text-justify text-[#7a7a7a]">PROMITIA WAS HERE AGAIN</p>
                         <p className="text-[14px] md:text-[16px] text-justify  text-[#7a7a7a] "><b>Little Villa</b> aporta activamente a la educación, buscando
                             permanentemente nuevas miradas de acercamiento a la primera infancia a través de
                             la reflexión y el cuestionamiento crítico, con el fin de brindar las mejores condiciones
